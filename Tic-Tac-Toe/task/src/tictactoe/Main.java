93dd2173ebde35777198b435eb44cd893fb9581e
Zaid Neurothrone <zaid_neurothrone@hotmail.com>
2021-01-29 18:43:46 +0100

    Complete Stage 1/5: Welcome to the battlefield!

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,7 +1,11 @@
 package tictactoe;
 
-public class Main {
-    public static void main(String[] args) {
-        // write your code here
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("X O X");
+        System.out.println("O X O");
+        System.out.println("X X O");
     }
 }
