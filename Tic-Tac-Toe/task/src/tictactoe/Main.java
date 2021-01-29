276268189ddd5612f701745d56b53876d43af7cc
Zaid Neurothrone <zaid_neurothrone@hotmail.com>
2021-01-29 18:49:55 +0100

    Complete Stage 2/5: The user is the gamemaster

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,11 +1,31 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scanner = new Scanner(System.in);
+        final int size = 3;
+        char[][] grid = new char[size][size];
+
+        System.out.print("Enter cells: ");
+        String userInput = scanner.nextLine();
+
+        // Draw grid with user input
+        System.out.println("---------");
+        int index = 0;
+        for (int row = 0; row < size; ++row)
+        {
+            System.out.print("| ");
+            for (int col = 0; col < size; ++col)
+            {
+                System.out.print(userInput.charAt(index++) + " ");
+            }
+            System.out.println("|");
+        }
+
+        System.out.println("---------");
     }
 }
