e567c10aa9f4cdea6acefe26f07a1333de6f4ea3
Zaid Neurothrone <zaid_neurothrone@hotmail.com>
2021-01-29 18:58:48 +0100

    Extract out the filling and printing of the 2D array into two separate methods

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -7,21 +7,55 @@ public class Main
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        final int size = 3;
-        char[][] grid = new char[size][size];
-
         System.out.print("Enter cells: ");
         String userInput = scanner.nextLine();
 
-        // Draw grid with user input
-        System.out.println("---------");
+        final int size = 3;
+        char[][] grid = convertInputToGrid(userInput, size);
+        printGrid(grid, size);
+    }
+
+    /**
+     * Returns a grid filled with X, O and _ chars
+     * from a String
+     *
+     * @param aInput String: the input that will fill the 2D array
+     * @param aSize int: the row and column size of the 2D array
+     * @return char[][]: the 2D char array
+     */
+    private static char[][] convertInputToGrid(String aInput, final int aSize)
+    {
+        char[][] grid = new char[aSize][aSize];
         int index = 0;
-        for (int row = 0; row < size; ++row)
+
+        for (int row = 0; row < aSize; ++row)
+        {
+            for (int col = 0; col < aSize; ++col)
+            {
+                grid[row][col] = aInput.charAt(index++);
+            }
+        }
+
+        return grid;
+    }
+
+    /**
+     * Prints the contents of the 2D char array passed
+     * as parameter
+     *
+     * @param aGrid char[][]: 2D char array filled with X, O and _ chars
+     * @param aSize int: the size of the row and columns of the 2D array
+     */
+    private static void printGrid(char[][] aGrid, final int aSize)
+    {
+        System.out.println("---------");
+
+        for (int row = 0; row < aSize; ++row)
         {
             System.out.print("| ");
-            for (int col = 0; col < size; ++col)
+            for (int col = 0; col < aSize; ++col)
             {
-                System.out.print(userInput.charAt(index++) + " ");
+                System.out.print(aGrid[row][col] + " ");
             }
             System.out.println("|");
         }
